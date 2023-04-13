65877d97537f6da5b079bb3b0a04edcfca49e38d
konono <kono@kononet.net>
2023-04-13 22:03:02 +0900

    [Mod] Split classes

--- holoscope/exporter_plugin/google_calendar.py
+++ holoscope/exporter_plugin/google_calendar.py
@@ -2,23 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import arrow
-import json
 import logging
-import os.path
-import pickle
 import re
 import socket
-import textwrap
 
-from ..datamodel import GCalEvent
-from ..datamodel import LiveEvent
-from ..token_manager import TokenManager
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from .. import utils
+from ..utils import GoogleCalendarUtils
 
 log = logging.getLogger(__name__)
 timeout_in_sec = 5
@@ -27,220 +16,105 @@ socket.setdefaulttimeout(timeout_in_sec)
 CALENDAR_API_SERVICE_NAME = 'calendar'
 CALENDAR_API_VERSION = 'v3'
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-
+TZ = "Asia/Tokyo"
 ISO861FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSSSSS'
-
-PAST = 7
+LINEFORMAT = 'YYYY/MM/DD HH:mm:ss'
+COLLAB_PATTERN = re.compile(r'^\[(.*?)\]')
 FUTURE = 120
 
 
 class Exporter(object):
     def __init__(self, config) -> None:
-        token_manager = TokenManager(config, token_type='google_calendar')
-        self.calendar = build(
-            CALENDAR_API_SERVICE_NAME,
-            CALENDAR_API_VERSION,
-            credentials=token_manager._get_token()
-        )
-        self.calendar_id = config.google_calendar.calendar_id
-        self.actual_end_time = config.google_calendar.enable_actual_end_time
         self.holomenbers = config.holodule.holomenbers
-        self.events = self._get_events()
-
-    @staticmethod
-    def _refresh_credential() -> Credentials:
-        # Google にcalendarへのアクセストークンを要求してcredsに格納します。
-        creds = None
-
-        # 有効なトークンをすでに持っているかチェック（２回目以降の実行時に認証を省略するため）
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
-                creds = pickle.load(token)
-
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            # アクセストークンを要求
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-                creds = flow.run_local_server(port=0)
-            # アクセストークン保存（２回目以降の実行時に認証を省略するため）
-            with open('token.pickle', 'wb') as token:
-                pickle.dump(creds, token)
-        return creds
-
-    def _get_events(self, past: int = PAST, future: int = FUTURE) -> list:
-        # 指定されたカレンダーからeventを取得
-        events = []
-        now = arrow.utcnow()
-        past = now.shift(days=-past).format(ISO861FORMAT) + 'Z'
-        future = now.shift(days=future).format(ISO861FORMAT) + 'Z'
-        try:
-            # Call the Calendar API
-            responses = self.calendar.events().list(calendarId=self.calendar_id,
-                                                    timeMin=(past),
-                                                    timeMax=(future),
-                                                    maxResults=250, singleEvents=True,
-                                                    orderBy='startTime').execute()
-            responses = responses.get('items', [])
-            if not responses:
-                log.error('Upcomming events was not found.')
-                return events
-            log.debug('CALENDAR EVENT JSON DUMP')
-            log.debug(json.dumps(responses))
-            for resp in responses:
-                events.append(GCalEvent(resp))
-                log.info(f'Schedule found {events[-1].title}.')
-            return events
-        except HttpError as error:
-            log.error(f'An error occurred: {error}.')
+        self.google_calendar = GoogleCalendarUtils(config)
+        self.events = self.google_calendar.get_events()
 
     def create_event(self, live_events: list) -> None:
         for live_event in live_events:
-            if live_event.collaborate:
-                title_str = (f'[{", ".join(live_event.collaborate)} コラボ] ' +
-                             f'{live_event.channel_title}: {live_event.title}')
-            else:
-                title_str = f'{live_event.channel_title}: {live_event.title}'
-            if live_event.actual_start_time:
-                start_dateTime = live_event.actual_start_time.format(ISO861FORMAT) + 'Z'
-                end_dateTime = live_event.actual_start_time.shift(hours=+1).format(ISO861FORMAT) + 'Z'
-                if live_event.actual_end_time and self.actual_end_time:
-                    end_dateTime = live_event.actual_end_time.format(ISO861FORMAT) + 'Z'
-            else:
-                start_dateTime = live_event.scheduled_start_time.format(ISO861FORMAT) + 'Z'
-                end_dateTime = live_event.scheduled_start_time.shift(hours=+1).format(ISO861FORMAT) + 'Z'
-            body = {
-                # 予定のタイトル
-                'summary': f'{title_str}',
-                'description':
-                textwrap.dedent(f'''
-                 チャンネル: {live_event.channel_title}
-                 タイトル: {live_event.title}
+            self.process_live_event(live_event)
 
-                  配信URL: https://www.youtube.com/watch?v={live_event.id}
-                '''),
-                # 予定の開始時刻
-                'start': {
-                    'dateTime': start_dateTime,
-                    'timeZone': 'Japan'
-                },
-                # 予定の終了時刻
-                'end': {
-                    'dateTime': end_dateTime,
-                    'timeZone': 'Japan'
-                },
-                'extendedProperties': {
-                    'private': {
-                        "video_id": live_event.id,
-                        "title": live_event.title,
-                        "channel_id": live_event.channel_id,
-                        "actor": live_event.actor,
-                        "collaborate": live_event.collaborate,
-                        "scheduled_start_time": live_event.scheduled_start_time.format(ISO861FORMAT)
-                    }
-                },
-            }
-            if (event := [event for event in self.events if live_event.id == event.video_id]):
-                event = event[0]
-                if title_str != event.title:
-                    self._update_event(event.id, live_event)
-                    log.info(f'[{live_event.id}]: Update the title of the scheduled {live_event.title}.')
-                    continue
-                if live_event.actual_start_time:
-                    if live_event.actual_start_time.to('Asia/Tokyo') != event.start_time:
-                        self._update_event(event.id, live_event)
-                        log.info(f'[{live_event.id}]: Update the start time to the actual time ' +
-                                 f'from the scheduled {live_event.title}.')
-                        continue
-                elif live_event.scheduled_start_time.to('Asia/Tokyo') != event.start_time:
-                    self._update_event(event.id, live_event)
-                    log.info(f'[{live_event.id}]: Update the start time ' +
-                             f'of the scheduled{live_event.title}.')
-                    continue
-                if live_event.actual_end_time:
-                    if live_event.actual_end_time.to('Asia/Tokyo') != event.end_time:
-                        self._update_event(event.id, live_event)
-                        log.info(f'[{live_event.id}]: Add the actual end time to ' +
-                                 f'the scheduled {live_event.title}.')
-                        continue
-                log.info(f'[{live_event.id}]: {live_event.title} is already scheduled.')
-            else:
-                # 2ヶ月以上将来の予定は予定に入れない
-                if live_event.scheduled_start_time > arrow.utcnow().shift(days=FUTURE):
-                    log.info(f'[{live_event.id}]: {title_str} was not scheduled, ' +
-                             f'because it is {FUTURE} days away.')
-                    continue
-                event = self.calendar.events().insert(calendarId=self.calendar_id,
-                                                      body=body).execute()
-                log.info(f'[{live_event.id}]: Create {title_str} has been scheduled.')
+    def process_live_event(self, live_event):
+        event = self.find_event(live_event)
+        title = utils.create_title(live_event)
 
-    def _update_event(self, event_id: str, live_event: LiveEvent):
-        if live_event.collaborate:
-            title_str = (f'[{", ".join(live_event.collaborate)} コラボ] ' +
-                         f'{live_event.channel_title}: {live_event.title}')
+        log.info(f'[{live_event.id}] ### Processing {live_event.title}.')
+        if event:
+            self.update_event_if_needed(event, live_event, title)
         else:
-            title_str = f'{live_event.channel_title}: {live_event.title}'
-        if live_event.actual_start_time:
-            start_dateTime = live_event.actual_start_time.format(ISO861FORMAT) + 'Z'
-            end_dateTime = live_event.actual_start_time.shift(hours=+1).format(ISO861FORMAT) + 'Z'
-            if live_event.actual_end_time and self.actual_end_time:
-                end_dateTime = live_event.actual_end_time.format(ISO861FORMAT) + 'Z'
-        else:
-            start_dateTime = live_event.scheduled_start_time.format(ISO861FORMAT) + 'Z'
-            end_dateTime = live_event.scheduled_start_time.shift(hours=+1).format(ISO861FORMAT) + 'Z'
-        body = {
-            # 予定のタイトル
-            'summary': f'{title_str}',
-            'description':
-            textwrap.dedent(f'''
-             チャンネル: {live_event.channel_title}
-             タイトル: {live_event.title}
+            self.create_event_if_possible(live_event, title)
 
-              配信URL: https://www.youtube.com/watch?v={live_event.id}
-            '''),
-            # 予定の開始時刻
-            'start': {
-                'dateTime': start_dateTime,
-                'timeZone': 'Japan'
-            },
-            # 予定の終了時刻
-            'end': {
-                'dateTime': end_dateTime,
-                'timeZone': 'Japan'
-            },
-            'extendedProperties': {
-                'private': {
-                    "video_id": live_event.id,
-                    "title": live_event.title,
-                    "channel_id": live_event.channel_id,
-                    "actor": live_event.actor,
-                    "collaborate": live_event.collaborate,
-                    "scheduled_start_time": live_event.scheduled_start_time.format(ISO861FORMAT)
-                }
-            },
-        }
-        self.calendar.events().update(calendarId=self.calendar_id,
-                                      eventId=event_id,
-                                      body=body).execute()
+    def find_event(self, live_event):
+        return next((event for event in self.events if live_event.id == event.video_id), None)
 
-    def delete_deplicate_event(self, live_events: list):
-        pattern = re.compile(r'^\[\D*\sコラボ\]')
-        for i in self.holomenbers:
+    def update_event_if_needed(self, event, live_event, title):
+        should_notify = False
+
+        if title != event.title:
+            self.google_calendar.update_event(event.id, live_event)
+            log.info(f'[{live_event.id}] [UPDATE]: [{event.id}] ' +
+                     f'Update title {live_event.title}.')
+            should_notify = True
+
+        if (live_event.actual_start_time and
+                live_event.actual_start_time.to(TZ) != event.start_dateTime):
+            self.google_calendar.update_event(event.id, live_event)
+            log.info(f'[{live_event.id}] [UPDATE]: [{event.id}] ' +
+                     f'Update to actual start_dateTime {live_event.title}.')
+            if not event.actual_start_time:
+                should_notify = True
+
+        if (not live_event.actual_start_time and
+                live_event.scheduled_start_time.to(TZ) != event.start_dateTime):
+            self.google_calendar.update_event(event.id, live_event)
+            log.info(f'[{live_event.id}] [UPDATE]: [{event.id}] ' +
+                     f'Update to scheduled start_dateTime {live_event.title}.')
+            should_notify = True
+
+        if live_event.scheduled_start_time.to(TZ) > arrow.utcnow().to(TZ):
+            if (live_event.scheduled_start_time.to(TZ) - arrow.utcnow().to(TZ)).seconds <= 900:
+                should_notify = True
+
+        if (live_event.actual_end_time and
+                live_event.actual_end_time.to(TZ) != event.end_dateTime):
+            self.google_calendar.update_event(event.id, live_event)
+            log.info(f'[{live_event.id}] [UPDATE]: [{event.id}] ' +
+                     f'Update to actual end_dateTime {live_event.title}.')
+            if not event.actual_end_time:
+                should_notify = True
+
+        if not should_notify:
+            log.info(f'[{live_event.id}] [ALREADY_EXIST]: [{event.id}] ' +
+                     f'{live_event.title} is already scheduled.')
+
+    def create_event_if_possible(self, live_event, title):
+        if live_event.scheduled_start_time > arrow.utcnow().shift(days=FUTURE):
+            log.info(f'[{live_event.id}]: {title} was not scheduled, ' +
+                     f'because it is {FUTURE} days away.')
+            return
+
+        created_event = self.google_calendar.create_event(live_event)
+        log.info(f'[{live_event.id}] [CREATE]: [{created_event.get("id")}] ' +
+                 f'Create {title} has been scheduled.')
+
+    def delete_duplicate_event(self, live_events: list):
+        for member in self.holomenbers:
             for live_event in live_events:
-                if not live_event.collaborate and live_event.actor == i:
-                    for e in self.events:
-                        match = pattern.match(e.title)
-                        if match:
-                            collabo_title = match.group()
-                        else:
-                            continue
-                        if i not in collabo_title:
-                            continue
-                        if e.scheduled_start_time:
-                            sst = live_event.scheduled_start_time
-                            if e.scheduled_start_time == sst:
-                                self.calendar.events().delete(calendarId=self.calendar_id,
-                                                              eventId=e.id).execute()
-                                log.info(f'[{e.id}] was deleted because deplicate {e.title}.')
+                if live_event.collaborate or live_event.actor != member:
+                    continue
+                for event in self.events:
+                    collaborater = self._get_collabo_title(event.title)
+                    if not collaborater or member not in collaborater:
+                        continue
+                    if event.scheduled_start_time == live_event.scheduled_start_time:
+                        self.google_calendar.delete_event(event.id, live_event)
+                        log.info(f'[{live_event.id}] [DELETE] [{event.id}] ' +
+                                 f'was deleted because of duplicate {event.title}.')
+
+    def _get_collabo_title(self, title: str) -> str:
+        match = COLLAB_PATTERN.search(title)
+        if match:
+            collabo_title = match.group(1)
+            collaborater = collabo_title.split()
+            collaborater.remove('コラボ')
+            return collaborater
+        return ''
--- holoscope/utils.py
+++ holoscope/utils.py
@@ -1,6 +1,67 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import arrow
+import boto3
+import json
+import logging
+import os.path
+import socket
+import textwrap
+
+from .datamodel import GCalEvent
+from .token_manager import TokenManager
+
+from datetime import datetime
+from datetime import timedelta
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from icalendar import Calendar
+from icalendar import Event
+
+from linebot.exceptions import LineBotApiError
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+
+
+log = logging.getLogger(__name__)
+timeout_in_sec = 5
+socket.setdefaulttimeout(timeout_in_sec)
+
+CALENDAR_API_SERVICE_NAME = 'calendar'
+CALENDAR_API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+TZ = "Asia/Tokyo"
+ISO861FORMAT = 'YYYY-MM-DDTHH:mm:ss.SSSSSS'
+LINEFORMAT = 'YYYY/MM/DD HH:mm:ss'
+PAST = 7
+FUTURE = 120
+
+
+def create_title(live_event):
+    if live_event.collaborate:
+        title = (f'[{" ".join(live_event.collaborate)} コラボ] ' +
+                 f'{live_event.channel_title}: {live_event.title}')
+    else:
+        title = f'{live_event.channel_title}: {live_event.title}'
+    return title
+
+
+def create_event_dateTime(live_event, time_format):
+    start_time = live_event.actual_start_time or live_event.scheduled_start_time
+    end_time = live_event.actual_end_time or start_time + timedelta(hours=1)
+
+    if time_format == LINEFORMAT:
+        start_dateTime = start_time.to(TZ).format(time_format)
+        end_dateTime = end_time.to(TZ).format(time_format)
+    elif time_format == ISO861FORMAT:
+        start_dateTime = start_time.format(time_format) + 'Z'
+        end_dateTime = end_time.format(time_format) + 'Z'
+
+    return start_dateTime, end_dateTime
+
 
 class YoutubeUtils():
     def __init__(self, youtube_instance):
@@ -14,7 +75,17 @@ class YoutubeUtils():
         video_ids = [item['id']['videoId'] for item in response.get('items', [])]
         return video_ids
 
-    def get_live_event(self, video_ids: list) -> list:
+    def get_live_event(self, video_id: list) -> list:
+        part = 'snippet,liveStreamingDetails'
+        video_response = self.youtube.videos().list(id=video_id,
+                                                    part=part).execute()
+        try:
+            video_response = video_response.get('items')[0]
+        except IndexError:
+            video_response = None
+        return video_response
+
+    def get_live_events(self, video_ids: list) -> list:
         part = 'snippet,liveStreamingDetails'
         video_response = self.youtube.videos().list(id=','.join(video_ids),
                                                     part=part).execute()
@@ -27,20 +98,190 @@ class YoutubeUtils():
         return response.get('items', [])
 
 
-class GooglecClendarUtils(object):
-    def __init__(self, google_calendar_instance) -> None:
-        self.youtube = google_calendar_instance
+class GoogleCalendarUtils:
+    def __init__(self, config):
+        self.calendar_id = config.google_calendar.calendar_id
+        token_manager = TokenManager(config, token_type='google_calendar')
+        self.calendar_service = build(
+            CALENDAR_API_SERVICE_NAME,
+            CALENDAR_API_VERSION,
+            credentials=token_manager._get_token())
 
-    def get_calendars(self) -> list:
-        # カレンダーのリストを取得
-        pt = None
-        calendars = []
+    def _create_event_data(self, live_event):
+        title = create_title(live_event)
+        start_dateTime, end_dateTime = create_event_dateTime(live_event, ISO861FORMAT)
+        # 予定のタイトル
+        summary = f'{title}'
+        description = textwrap.dedent(f'''
+        チャンネル: {live_event.channel_title}
+        タイトル: {live_event.title}
 
-        while True:
-            calendar_list = self.calendar.calendarList().list(pageToken=pt).execute()
-            for calendar_list_entry in calendar_list['items']:
-                calendars.append(calendar_list_entry['summary'])
-            page_token = calendar_list.get('nextPageToken')
-            if not page_token:
-                break
-        return calendars
+        配信URL: https://www.youtube.com/watch?v={live_event.id}
+        '''[1:-1])
+        # 予定の開始時刻
+        start_time = {
+            'dateTime': start_dateTime,
+            'timeZone': TZ
+        }
+        end_time = {
+                'dateTime': end_dateTime,
+                'timeZone': TZ
+            }
+        extended_property = {
+            "private": {
+                "video_id": live_event.id,
+                "title": live_event.title,
+                "channel_id": live_event.channel_id,
+                "actor": live_event.actor,
+                "scheduled_start_time": live_event.scheduled_start_time.format(ISO861FORMAT),
+            }
+        }
+        if live_event.collaborate:
+            extended_property["private"]["collaborate"] = " ".join(live_event.collaborate)
+        if live_event.actual_start_time:
+            extended_property["private"]["actual_start_time"] = live_event.actual_start_time.\
+                    format(ISO861FORMAT)
+        if live_event.actual_end_time:
+            extended_property["private"]["actual_end_time"] = live_event.actual_end_time.\
+                    format(ISO861FORMAT)
+        body = {
+                'summary': summary,
+                'description': description,
+                'start': start_time,
+                'end': end_time,
+                'extendedProperties': extended_property
+            }
+        return body
+
+    def create_event(self, live_event):
+        body = self._create_event_data(live_event)
+        try:
+            created_event = self.calendar_service.events().insert(
+                    calendarId=self.calendar_id, body=body).execute()
+            log.info(f'[{live_event.id}]: Event created {created_event.get("htmlLink")}')
+        except HttpError as error:
+            log.info(f'An error occurred: {error}')
+            created_event = None
+        return created_event
+
+    def update_event(self, event_id, live_event):
+        body = self._create_event_data(live_event)
+        try:
+            updated_event = self.calendar_service.events().update(
+                    calendarId=self.calendar_id, eventId=event_id, body=body).execute()
+            log.info(f'[{live_event.id}]: Event updated {live_event.title}')
+            log.info(f'[{live_event.id}]: Event updated url is {updated_event.get("htmlLink")}')
+        except HttpError as error:
+            log.info(f'An error occurred: {error}')
+            updated_event = None
+        return updated_event
+
+    def delete_event(self, event_id, live_event):
+        try:
+            deleted_event = self.calendar_service.events().delete(
+                    calendarId=self.calendar_id, eventId=event_id).execute()
+            log.info(f'[{live_event.id}]: Event deleted {live_event.title}')
+            log.info(f'[{live_event.id}]: Event deleted url is {deleted_event.get("htmlLink")}')
+        except HttpError as error:
+            log.info(f'An error occurred: {error}')
+            deleted_event = None
+        return deleted_event
+
+    def get_events(self, past: int = PAST, future: int = FUTURE) -> list:
+        # 指定されたカレンダーからeventを取得
+        events = []
+        now = arrow.utcnow()
+        past = now.shift(days=-past).format(ISO861FORMAT) + 'Z'
+        future = now.shift(days=future).format(ISO861FORMAT) + 'Z'
+        try:
+            # Call the Calendar API
+            responses = self.calendar_service.events().list(calendarId=self.calendar_id,
+                                                            timeMin=(past),
+                                                            timeMax=(future),
+                                                            maxResults=250, singleEvents=True,
+                                                            orderBy='startTime').execute()
+            responses = responses.get('items', [])
+            if not responses:
+                log.error('Upcomming events was not found.')
+                return events
+            log.debug('CALENDAR EVENT JSON DUMP')
+            log.debug(json.dumps(responses))
+            for resp in responses:
+                events.append(GCalEvent(resp))
+                if(events[-1].scheduled_start_time.to(TZ) > arrow.utcnow().to(TZ)):
+                    log.info(f'Schedule found {events[-1].title}.')
+            return events
+        except HttpError as error:
+            log.error(f'An error occurred: {error}.')
+
+
+class LineMessageSender:
+    def __init__(self, config):
+        self.linebot = LineBotApi(config.line.line_channel_access_token)
+
+    def create_message_data(self, live_event):
+        title = create_title(live_event)
+        start_dateTime, end_dateTime = create_event_dateTime(live_event, LINEFORMAT)
+        line_message = f'タイトル: {title}\n'\
+                       f'チャンネル: {live_event.channel_title}\n'\
+                       f'開始時刻: {start_dateTime}\n'\
+                       f'配信URL: https://www.youtube.com/watch?v={live_event.id}'
+        return line_message
+
+    def broadcast_message(self, line_message):
+        try:
+            self.linebot.broadcast(TextSendMessage(text=line_message))
+        except LineBotApiError as e:
+            log.error(f'LineBotApiError: {e}.')
+
+
+class S3Utils:
+    def __init__(self, config):
+        self.s3 = boto3.client('s3',
+                               aws_access_key_id=config.aws.access_key_id,
+                               aws_secret_access_key=config.aws.secret_access_key)
+        self.s3_bucket = config.aws.s3_bucket
+
+    def _create_ics(self, live_event) -> str:
+        title = create_title(live_event)
+        start_dateTime, end_dateTime = create_event_dateTime(live_event, LINEFORMAT)
+        cal = Calendar()
+        cal.creator = 'Hololine'
+        cal.add('prodid', '-//Okayun Calendar//product//ja//')
+        cal.add('version', '2.0')
+        cal.add('calscale', 'GREGORIAN')
+        cal.add('method', 'REQUEST')
+
+        # イベント作成
+        event = Event()
+        event.add('summary', f'{title}')
+        event.add('dtstart', datetime.strptime(start_dateTime, '%Y/%m/%d %H:%M:%S'))
+        event.add('dtend', datetime.strptime(end_dateTime, '%Y/%m/%d %H:%M:%S'))
+        event.add('description', textwrap.dedent(f'''
+        タイトル: {title}
+        チャンネル: {live_event.channel_title}
+        配信URL: https://www.youtube.com/watch?v={live_event.id}
+        '''[1:-1]))
+        cal.add_component(event)
+
+        # icsファイル作成
+        path = f'/tmp/{live_event.id}.ics'
+        f = open(path, 'wb')
+        f.write(cal.to_ical())
+        f.close()
+        return path
+
+    def _ics_upload(self, path) -> str:
+        file_name = os.path.basename(path)
+        self.s3.upload_file(path, self.s3_bucket, file_name)
+        return file_name
+
+    def create_presigned_url(self, live_event) -> str:
+        path = self._create_ics(live_event)
+        bucket_key = self._ics_upload(path)
+        presigned_url = self.s3.generate_presigned_url(
+            ClientMethod='get_object',
+            Params={'Bucket': self.s3_bucket, 'Key': bucket_key},
+            ExpiresIn=86400,
+            HttpMethod='GET')
+        return presigned_url
